7cb13744b511ebbffa86630c9ff845cee1aac35e
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-06-14 11:32:24 -0500

    adds credits to badge collector

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -21,6 +21,7 @@ function generateMarkdown(data) {
   let tableOfContents = "## Table of Contents\n\n";
 
   switch (license) {
+    //thanks to Lukas Himsel on GitHub for collecting the badges (https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)
     case "MIT License":
       badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
       licenseText = `Copyright ${year} ${name}\n
